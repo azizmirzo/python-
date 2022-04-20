ae1c0b6fc11076e88b506042cec486541146bd6a
Abdulaziz <93951510+azizmirzo@users.noreply.github.com>
2022-04-20 18:49:14 +0500

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,63 @@
+import imp
+from turtle import left, pendown, penup
+from turtlepenup,  importpenup,  
+ 
+speed(100)
+bgcolor('#fb3958')
+pencolor('white')
+pensize(10)
+
+penup()
+goto(100,100)
+pendown()
+
+left(180)
+
+def borde():
+    forward(150)
+    for i in renge(90):
+        forward(1)
+        left(1)
+
+border()
+border()
+border()
+border()
+
+penup()
+for i in renge(90):
+    backward(1)
+    right(1)
+    left(90)
+    pendown()
+    
+forward(259)      
+    
+penup()
+right(180)
+forward(85)
+left(120)
+pendown()
+
+forward(60)
+right(120)
+
+penup()
+forward(100)
+right(60)
+pendown()
+
+forward(60)
+
+penup()
+goto(5,-20)
+left(60)
+right(90)
+pendown()
+
+for i in renge(3):
+    forward(70)
+    left(120)
+ 
+hideturtle()
+done()    
